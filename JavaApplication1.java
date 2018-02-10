2ef5f811e30e6b4f3d14dc0e76cb3a789a5a5e86
Ayşegül Tan <keyikcik@gmail.com>
2018-02-10 22:04:02 +1300

    1
    first codes

---
+++ JavaApplication1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author kyk
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World! I am Arya :)");
+        int a = 10;
+        int b=a;
+        a=100;
+        System.out.println("a: "+a+" b:"+b);
+                     
+                
+        
+               
+    }
+    
+}
